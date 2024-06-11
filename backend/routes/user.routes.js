5d50860fb953f4104745b3f17baca4816a2ca6f6
Aditya Singh <adi750787@gmail.com>
2024-06-11 16:28:21 +0530

    first part completed - backend

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getUsersForSidebar); 
+// protectRoute makes sure that unauthorised users cannot cALL THE next function 
+
+export default router;
