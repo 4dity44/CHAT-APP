79889ef25347929c98a92b2bab78a1e1c61463fb
Aditya Singh <adi750787@gmail.com>
2024-06-11 19:30:46 +0530

    2nd part done - ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='chat chatr-end'>
+        <div className='chat-image avatar'>
+
+
+        </div>
+        </div>
+  )
+}
+
+export default Message
